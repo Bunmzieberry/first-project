523403ac5ad26f17d39975f12f128db2c4fab479
oluwabunmi onifade <onifadeoluwa514@gmail.com>
2022-03-14 13:18:39 +0100

    Edit on the logo/name

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ var swiper = new Swiper(".home-slider", {
   spaceBetween: 30,
   centeredSlides: true,
   autoplay: {
-    delay: 4500,
+    delay: 3500,
     disableOnInteraction: false,
   },
   pagination: {
